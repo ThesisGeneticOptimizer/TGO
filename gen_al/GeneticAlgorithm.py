0ea330ef6cc3a850e2aa6a9fb8b2dcaa4ac65de9
judeescol1997 <34189306+judeescol1997@users.noreply.github.com>
2017-12-28 00:15:06 +0800

    Update GeneticAlgorithm.py

--- gen_al/GeneticAlgorithm.py
+++ gen_al/GeneticAlgorithm.py
@@ -1,9 +1,11 @@
 import random
-import re
 from Matrix import GuestPair
 
 parents = []
 offsprings = []
+
+optimal = []
+
 def initialization(guest, table):
     print("====INITIALIZATION====\nRandom seating assignments")
     for t in range(0,len(table)):
@@ -17,71 +19,61 @@ def initialization(guest, table):
             except:
                 pass
 
-def evaluation(table, nexts, keys):
-    print("\n\n====EVALUATION====")
-   
-    for t in range(0, len(table)):
-        print("Table",(t+1))
-        for i in range(0, len(table[t].chairs)-1):
-            for j in range(nexts, len(table[t].chairs)):
-                print("-> ",table[t].chairs[i],"-",table[t].chairs[j]," = ",GuestPair.getPoints(table[t].chairs[i],table[t].chairs[j]))
-                keys+=str((GuestPair.getPoints(table[t].chairs[i],table[t].chairs[j])))
-            nexts+=1
-        nexts = 1
-        calculateFitness(keys,0,0,[],0,["-","+"])
-        keys = ""
+def evaluation(table, next, keys):
+    try:
+        print("\n\n====EVALUATION====")
+        for t in range(0, len(table)):
+            print("Table",(t+1),"("+str(table[t].generation)+")")
+            for i in range(0, len(table[t].chairs)-1):
+                for j in range(next, len(table[t].chairs)):
+                    print("-> ",table[t].chairs[i],"-",table[t].chairs[j]," = ",GuestPair.getPoints(table[t].chairs[i],table[t].chairs[j]))
+                    keys+=str(GuestPair.getPoints(table[t].chairs[i], table[t].chairs[j]))
+                next+=1
+            next = 1
+            calculateFitness(keys,0,0,[],0)
+            keys = ""
+    except:
+        pass
 
 def crossover(table,i):
-    print("\n\n====CROSSOVER====")
-    while i < len(table):
-        print("-> selected parents:",table[i].chairs,table[i+1].chairs)
-        offSpring1 = table[i].chairs[:int(len(table[i].chairs)/2)+1] + table[i+1].chairs[int(len(table[i].chairs)/2)+1:]
-        offSpring2 = table[i].chairs[int(len(table[i].chairs) / 2)+1:] + table[i + 1].chairs[:int(len(table[i].chairs)/2)+1]
-        table[i].setOffSpring(offSpring1)
-        table[i+1].setOffSpring(offSpring2)
-        print("= produced offspring",offSpring1,offSpring2)
-        i+=2
-    evaluation(table,1,"")
-    mutation(table,offSpring1,offSpring2)
-
-def mutation(table,offSpring1,offSpring2):
-    print("\n\n===MUTATION===")
-    x = random.randint(0,len(offSpring1)-1)
-    y = random.randint(0,len(offSpring1)-1)
-    offSpring1[x],offSpring1[y] = offSpring1[y] , offSpring1[x]
-
-    x = random.randint(0,len(offSpring2)-1)
-    y = random.randint(0,len(offSpring2)-1)
-    offSpring2[x],offSpring2[y] = offSpring2[y] , offSpring2[x]
-
-    
-    print(offSpring1)
-    print(offSpring2)
-     
-    table[0].setOffSpring(offSpring1)
-    table[1].setOffSpring(offSpring2)
-
-    evaluation(table,1,"")
-    
-def calculateFitness(keys,i,s,temp,final,operator):
-    try:    
-        if(len(keys)%2==1):
-            keys+="0"
-        while i < len(keys):
-            temp.append(int(keys[i]) + int(keys[i+1]))
+    try:
+        print("\n\n====CROSSOVER====")
+        while i < len(table):
+            print("-> selected parents:",table[i].chairs, table[i+1].chairs)
+            offSpring1 = table[i].chairs[:int(len(table[i].chairs)/2)+1] + table[i+1].chairs[int(len(table[i].chairs)/2)+1:]
+            offSpring2 = table[i].chairs[int(len(table[i].chairs) / 2)+1:] + table[i+1].chairs[:int(len(table[i].chairs)/2)+1]
+            table[i].setOffSpring(offSpring1)
+            table[i+1].setOffSpring(offSpring2)
+            print("= produced offspring",offSpring1,offSpring2)
             i+=2
+        evaluation(table,1,"")
+    except:
+        pass
 
-        print(temp,keys)
-        final+=int(temp[0]);
+
+def calculateFitness(keys, i, s, temp, final):
+    try:
+        if (len(keys) % 2 == 1):
+            keys += "0"
+        while i < len(keys):
+            temp.append(int(keys[i]) + int(keys[i + 1]))
+            i += 2
+
+        print(temp, keys)
+        final += int(temp[0])
         for j in range(1, len(temp)):
-            if(s==0):
-                final-=int(temp[j])
+            if (s == 0):
+                final -= int(temp[j])
                 s = 1
             else:
-                final+=int(temp[j])
+                final += int(temp[j])
                 s = 0
-        
         print(str(final))
     except Exception as e:
         print(e)
-    
+
+def shuffled(x):
+    y = x[:]
+    random.shuffle(y)
+    return y
+

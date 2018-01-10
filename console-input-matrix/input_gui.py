e14b2a9ccbe132301126f89f70d63b627dfb3fa0
edgieace <30334056+edgieace@users.noreply.github.com>
2018-01-10 17:05:45 +0800

    Update input_gui.py
    -- Added GUI with scrollbar for showing big datas

--- console-input-matrix/input_gui.py
+++ console-input-matrix/input_gui.py
@@ -6,6 +6,25 @@ from tkinter import *
 root = tkinter.Tk()
 menubar = Menu(root)
 filemenu = Menu(menubar, tearoff = 0)
+root.geometry("1000x1000")
+
+Can1 = tkinter.Canvas(root, bg="Gray")
+Can1.grid(row=0, column=0)
+
+vsbar = tkinter.Scrollbar(root, orient="vertical", command=Can1.yview)
+vsbar.grid(row=0, column=1, sticky=N+S+E+W)
+Can1.configure(yscrollcommand=vsbar.set)
+
+hsbar = tkinter.Scrollbar(root, orient="horizontal", command=Can1.xview)
+hsbar.grid(row=1, column=0, sticky=N+S+E+W)
+Can1.configure(xscrollcommand=hsbar.set)
+
+frame_buttons = tkinter.Frame(Can1, bg="Gray", bd=2, relief=tkinter.GROOVE)
+Can1.create_window((0,0), window=frame_buttons,anchor='nw')
+
+def resize(event):
+    Can1.configure(scrollregion=Can1.bbox("all"), width=980, height=690)
+frame_buttons.bind("<Configure>", resize)
 
 def display_matrix(fname):
     with open(fname,newline="") as file:
@@ -17,11 +36,11 @@ def display_matrix(fname):
           c = 0
           for row in col:
         
-             label = tkinter.Label(root, width = 10, height = 2, \
+             label = tkinter.Label(frame_buttons, width = 10, height = 2, \
                                    text = row, relief = tkinter.RIDGE)
              if row == "":
                 label.config(bg="gray")
-             label.grid(row = r, column = c)
+             label.grid(row = r, column = c,sticky='news')
              c += 1
           r += 1
       
